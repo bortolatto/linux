5f5489fd89474978fe3d8fdde9068d5db85c3626
Eduardo Bortolatto Lopes <48414886+bortolatto@users.noreply.github.com>
2021-12-03 22:43:00 -0300

    Update bagunca.sh

--- bagunca.sh
+++ bagunca.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-NUMERO_REGISTROS=$1
-NOME_ARQUIVO=$2
-NUMERO_INICIAL=$3
+usage() {
+   echo "Modo de usar: " >&2
+   echo "    ${0} -n{quantidade} -f{arquivo} -ni{semente}" >&2
+   echo "      -n  Quantidade de arquivos a serem gerados no arquivo" >&2
+   echo "      -f  Nome do arquivo json que foi exportado o avro (sem formatacao)" >&2
+   echo "      -ni Número da CDA inicial para que o script gere números a partir deste" >&2
+   exit 1
+}
+
+if [[ -n "${@}" || "${OPTIND}" -eq 1 ]]
+then
+   usage
+fi
+
+while getopts n:f:ni: OPCAO 
+do
+  case ${OPCAO} in
+    n) NUMERO_REGISTROS=${OPTARG} ;;
+    f) NOME_ARQUIVO=${OPTARG} ;;
+    ni) NUMERO_INICIAL=${OPTARG} ;;
+    ?) usage 
+       ;;
+  esac
+done
+
+echo $OPTIND
 
 while [[ 1 -le $NUMERO_REGISTROS ]]
 do
